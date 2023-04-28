bb01be5e546452aa4bc3095d3dd3b9f3b8e55eb0
Omar Abdellah <118400437+oabdellah@users.noreply.github.com>
2023-04-28 13:42:28 -0400

    Adding starter code for the simple pong game.

---
+++ simple_pong_game.py
@@ -0,0 +1,76 @@
+import pygame
+import sys
+
+# Initialize Pygame
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 800, 600
+PADDLE_WIDTH, PADDLE_HEIGHT = 15, 100
+BALL_SIZE = 15
+PADDLE_SPEED = 7
+BALL_SPEED = 5
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Create the screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Pong')
+
+# Create paddles and ball
+paddle_a = pygame.Rect(10, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+paddle_b = pygame.Rect(WIDTH - 20, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+ball = pygame.Rect(WIDTH // 2 - BALL_SIZE // 2, HEIGHT // 2 - BALL_SIZE // 2, BALL_SIZE, BALL_SIZE)
+
+# Ball velocity
+ball_speed_x = BALL_SPEED
+ball_speed_y = BALL_SPEED
+
+# Main game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Paddle movement
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and paddle_a.top > 0:
+        paddle_a.y -= PADDLE_SPEED
+    if keys[pygame.K_s] and paddle_a.bottom < HEIGHT:
+        paddle_a.y += PADDLE_SPEED
+    if keys[pygame.K_UP] and paddle_b.top > 0:
+        paddle_b.y -= PADDLE_SPEED
+    if keys[pygame.K_DOWN] and paddle_b.bottom < HEIGHT:
+        paddle_b.y += PADDLE_SPEED
+
+    # Ball movement
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    # Ball collision with walls
+    if ball.top <= 0 or ball.bottom >= HEIGHT:
+        ball_speed_y = -ball_speed_y
+
+    # Ball collision with paddles
+    if ball.colliderect(paddle_a) or ball.colliderect(paddle_b):
+        ball_speed_x = -ball_speed_x
+
+    # Ball out of bounds
+    if ball.left <= 0 or ball.right >= WIDTH:
+        ball.x = WIDTH // 2 - BALL_SIZE // 2
+        ball.y = HEIGHT // 2 - BALL_SIZE // 2
+        ball_speed_x = -ball_speed_x
+
+    # Drawing objects
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, paddle_a)
+    pygame.draw.rect(screen, WHITE, paddle_b)
+    pygame.draw.ellipse(screen, WHITE, ball)
+    pygame.draw.aaline(screen, WHITE, (WIDTH // 2, 0), (WIDTH // 2, HEIGHT))
+
+    # Update display
+    pygame.display.flip()
+    pygame.time.delay(20)
