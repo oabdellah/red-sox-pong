c278f9ff0b02d2a0fef62ff7662c68d0ee3c7293
Omar Abdellah <118400437+oabdellah@users.noreply.github.com>
2023-05-04 21:21:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pygame
-import sys
 from game import Game
 from game_modes import ClassicPong, TeamPong
 
-if __name__ == "__main__":
+
+def main():
+    """
+    Main function to run the Red "SOX" Pong game.
+    """
     game = Game()
     classic_pong = ClassicPong(game)
     team_pong = TeamPong(game)
-    game.home_page(classic_pong, team_pong)
+
+    while True:
+        game.home_page(classic_pong, team_pong)
+
+
+if __name__ == "__main__":
+    main()
